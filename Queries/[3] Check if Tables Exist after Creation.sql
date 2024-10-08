48faccc15d22dbf88bfeb73269b3ffe370ecdc51
MaxNardone <maxnardone3@gmail.com>
2024-10-08 10:51:52 +0100

    Create [3] Check if Tables Exist after Creation.sql

---
+++ Queries/[3] Check if Tables Exist after Creation.sql
@@ -0,0 +1,24 @@
+USE AssignmentPart1
+GO
+
+--CHECK if the Tables are populated
+SELECT *
+FROM Dbo.Location
+
+SELECT *
+FROM Dbo.Customer
+
+SELECT *
+FROM Dbo.OrderGroup
+
+SELECT *
+From Dbo.Product
+
+SELECT *
+From Dbo.ProductVariant
+
+SELECT *
+From Dbo.ProductMapping
+
+SELECT *
+FROM Dbo.OrderItem
